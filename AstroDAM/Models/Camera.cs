66de5282046d91010f46eadff9fd0718d5edd973
RedPhantom <mirpaa.itay@gmail.com>
2020-09-23 08:15:37 +0300

    Expanded documentation

--- AstroDAM/Models/Camera.cs
+++ AstroDAM/Models/Camera.cs
@@ -8,14 +8,28 @@ using System.Threading.Tasks;
 
 namespace AstroDAM.Models
 {
+    /// <summary>
+    /// Represents a camera that can be connected to a scope.
+    /// </summary>
     [DataContract]
     public class Camera
     {
+        /// <summary>
+        /// Initialize a new camera.
+        /// </summary>
         public Camera()
         {
             ColorSpaces = new List<ColorSpace>();
         }
 
+        /// <summary>
+        /// Initialize a new camera.
+        /// </summary>
+        /// <param name="id">The unique identifier of the camera in the database.</param>
+        /// <param name="shortName">The short, usual name of the camera.</param>
+        /// <param name="longName">The long, technical name of the camera.</param>
+        /// <param name="maxResolution">The maximum resolution the camera can capture.</param>
+        /// <param name="colorSpaces">The color spaces the camera can capture.</param>
         public Camera(int id, string shortName, string longName, Size maxResolution, List<ColorSpace> colorSpaces)
         {
             Id = id;
@@ -25,29 +39,42 @@ namespace AstroDAM.Models
             ColorSpaces = colorSpaces;
         }
 
-
-
         /// <summary>
-        /// Such as 0.
+        /// Unique identifier of the camera in the database.
         /// </summary>
+        /// <example>
+        /// 0
+        /// </example>
         [DataMember]
         public int Id { get; set; }
 
         /// <summary>
-        /// Such as "ASI120MC".
+        /// The short, usual name of the camera.
         /// </summary>
+        /// <example>
+        /// "ASI120MC"
+        /// </example>
         [DataMember]
         public string ShortName { get; set; }
 
         /// <summary>
-        /// Such as "Zwo ASI 120MC".
+        /// The long, technical name of the camera.
         /// </summary>
+        /// <example>
+        /// "Zwo ASI 120MC"
+        /// </example>
         [DataMember]
         public string LongName { get; set; }
 
+        /// <summary>
+        /// The maximum resolution the camera can capture.
+        /// </summary>
         [DataMember]
         public Size MaxResolution { get; set; }
 
+        /// <summary>
+        /// The color spaces the camera can capture.
+        /// </summary>
         [DataMember]
         public List<ColorSpace> ColorSpaces { get; set; }
     }
--- AstroDAM/Models/Catalogue.cs
+++ AstroDAM/Models/Catalogue.cs
@@ -7,11 +7,23 @@ using System.Threading.Tasks;
 
 namespace AstroDAM.Models
 {
+    /// <summary>
+    /// Represents a catalogue of space objects (stars, nebulas etc.)
+    /// </summary>
     [DataContract]
     public class Catalogue
     {
+        /// <summary>
+        /// Initialize a new catalogue.
+        /// </summary>
         public Catalogue() { }
 
+        /// <summary>
+        /// Initialize a new catalogue.
+        /// </summary>
+        /// <param name="id">Unique identifier of the camera in the database.</param>
+        /// <param name="shortName">The short, abbreviated name of the catalogue.</param>
+        /// <param name="longName">The long, technical name of the catalogue.</param>
         public Catalogue(int id, string shortName, string longName)
         {
             Id = id;
@@ -20,20 +32,29 @@ namespace AstroDAM.Models
         }
 
         /// <summary>
-        /// Such as 0.
+        /// Unique identifier of the catalogue in the database.
         /// </summary>
+        /// <example>
+        /// 0
+        /// </example>
         [DataMember]
         public int Id { get; set; }
 
         /// <summary>
-        /// Such as "M".
+        /// The short, abbreviated name of the camera.
         /// </summary>
+        /// <example>
+        /// "M"
+        /// </example>
         [DataMember]
         public string ShortName { get; set; }
 
         /// <summary>
-        /// Such as "Messier".
+        /// The long, technical name of the catalogue.
         /// </summary>
+        /// <example>
+        /// "Messier"
+        /// </example>
         [DataMember]
         public string LongName { get; set; }
     }
